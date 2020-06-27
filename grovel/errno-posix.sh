57bdd2a081d51f1a48e64f548e629bb3004fcff0
Lassi Kortela <lassi@lassi.io>
2020-06-27 18:54:19 +0300

    Integrate errno list into grovel.scm
    We have demonstrated that distinguishing between POSIX and non-POSIX
    errors is not useful in practice. Hence we can fold the combined
    listing of all those errors directly into the Scheme file, producing
    simpler code.

--- grovel/errno-posix.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -eu -o pipefail
-cd "$(dirname "$0")"
-curl --fail --silent --show-error \
-    https://pubs.opengroup.org/onlinepubs/9699919799/basedefs/errno.h.html |
-    grep -E '<dt>\[E[A-Z0-9]+\]</dt>' |
-    grep -oE 'E[A-Z0-9]+' >errno-posix.text
