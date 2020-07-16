26f8f748a2ca7e1161a978b1747a36e62f8d6017
Harold Ancell <hga@ancell-ent.com>
2020-07-16 10:57:38 -0500

    Skeleton of SRFI's expanded scope to foreign interfaces

--- srfi/chibi-scheme/lib/srfi/198.sld
+++ srfi/chibi-scheme/lib/srfi/198.sld
@@ -3,57 +3,21 @@
 (define-library (srfi 198)
   (export
 
-   ;; 3.1  Errors
+   foreign-error?
 
-#| useful for debuging
+   foreign-error:error-set foreign-error:code
+   foreign-error:scheme-procedure foreign-error:foreign-interface
+   foreign-error:locales foreign-error:message
+   foreign-error:data
 
-   errno/E2BIG errno/EACCES errno/EADDRINUSE errno/EADDRNOTAVAIL
-   errno/EAFNOSUPPORT errno/EAGAIN errno/EALREADY errno/EBADF
-   errno/EBADMSG errno/EBUSY errno/ECANCELED errno/ECHILD
-   errno/ECONNABORTED errno/ECONNREFUSED errno/ECONNRESET
-   errno/EDEADLK errno/EDESTADDRREQ errno/EDOM errno/EDQUOT
-   errno/EEXIST errno/EFAULT errno/EFBIG errno/EHOSTUNREACH
-   errno/EIDRM errno/EILSEQ errno/EINPROGRESS errno/EINTR errno/EINVAL
-   errno/EIO errno/EISCONN errno/EISDIR errno/ELOOP errno/EMFILE
-   errno/EMLINK errno/EMSGSIZE errno/ENAMETOOLONG errno/ENETDOWN
-   errno/ENETRESET errno/ENETUNREACH errno/ENFILE errno/ENOBUFS
-   errno/ENODEV errno/ENOENT errno/ENOEXEC errno/ENOLCK errno/ENOMEM
-   errno/ENOMSG errno/ENOPROTOOPT errno/ENOSPC errno/ENOSYS
-   errno/ENOTCONN errno/ENOTDIR errno/ENOTEMPTY errno/ENOTRECOVERABLE
-   errno/ENOTSOCK errno/ENOTSUP errno/ENOTTY errno/ENXIO
-   errno/EOPNOTSUPP errno/EOVERFLOW errno/EOWNERDEAD errno/EPERM
-   errno/EPIPE errno/EPROTO errno/EPROTONOSUPPORT errno/EPROTOTYPE
-   errno/ERANGE errno/EROFS errno/ESPIPE errno/ESRCH errno/ESTALE
-   errno/ETIMEDOUT errno/ETXTBSY errno/EWOULDBLOCK errno/EXDEV
-|#
-
-   make-syscall-error syscall-error?
-   syscall-error:errno syscall-error:message
-   syscall-error:procedure-name syscall-error:syscall-name
-   syscall-error:data
-   errno-error
-   errno-string errno-name
+   make-foreign-error
+   raise-foreign-error
    )
-
-  (cond-expand ((not openbsd)
-    (export
-
-#|
-     ;; 3.1  Errors
-
-     errno/EMULTIHOP errno/ENOLINK
-     ;; STREAMS:
-     errno/ENODATA errno/ENOSTR errno/ENOSR errno/ETIME
-|#
-    )))
 
   (import
    (scheme base)
 
-   (only (srfi 69) make-hash-table hash-table-set! hash-table-ref)
-   (srfi 198 common)
    )
 
-  (include-shared "198/198")
   (include "198/198.scm")
   )
--- srfi/chibi-scheme/lib/srfi/198/198.scm
+++ srfi/chibi-scheme/lib/srfi/198/198.scm
@@ -1,88 +1,20 @@
 ;; please see copyright notice in ./COPYING
 
-(define errno-map (make-hash-table))
+(define-record-type Foreign-Interface-Error
+    (make-foreign-interface-error-error error-set code scheme-procedure foreign-interface locales message data)
+    foreign-error?
+  (error-set foreign-error:error-set)
+  (code foreign-error:code)
+  (scheme-procedure foreign-error:scheme-procedure)
+  (foreign-interface foreign-error:foreign-interface)
+  (locales foreign-error:locales)
+  (message foreign-error:message)
+  (data foreign-error:data))
 
-(map (lambda (errno-number errno-symbol) (hash-table-set! errno-map errno-number errno-symbol))
-     (list
-      errno/E2BIG errno/EACCES errno/EADDRINUSE errno/EADDRNOTAVAIL
-      errno/EAFNOSUPPORT errno/EAGAIN errno/EALREADY errno/EBADF
-      errno/EBADMSG errno/EBUSY errno/ECANCELED errno/ECHILD
-      errno/ECONNABORTED errno/ECONNREFUSED errno/ECONNRESET
-      errno/EDEADLK errno/EDESTADDRREQ errno/EDOM errno/EDQUOT
-      errno/EEXIST errno/EFAULT errno/EFBIG errno/EHOSTUNREACH
-      errno/EIDRM errno/EILSEQ errno/EINPROGRESS errno/EINTR errno/EINVAL
-      errno/EIO errno/EISCONN errno/EISDIR errno/ELOOP errno/EMFILE
-      errno/EMLINK errno/EMSGSIZE errno/ENAMETOOLONG errno/ENETDOWN
-      errno/ENETRESET errno/ENETUNREACH errno/ENFILE errno/ENOBUFS
-      errno/ENODEV errno/ENOENT errno/ENOEXEC errno/ENOLCK errno/ENOMEM
-      errno/ENOMSG errno/ENOPROTOOPT errno/ENOSPC errno/ENOSYS
-      errno/ENOTCONN errno/ENOTDIR errno/ENOTEMPTY errno/ENOTRECOVERABLE
-      errno/ENOTSOCK errno/ENOTSUP errno/ENOTTY errno/ENXIO
-      errno/EOPNOTSUPP errno/EOVERFLOW errno/EOWNERDEAD errno/EPERM
-      errno/EPIPE errno/EPROTO errno/EPROTONOSUPPORT errno/EPROTOTYPE
-      errno/ERANGE errno/EROFS errno/ESPIPE errno/ESRCH errno/ESTALE
-      errno/ETIMEDOUT errno/ETXTBSY errno/EWOULDBLOCK errno/EXDEV
+(define (make-foreign-error)
+  #f
+  )
 
-      (cond-expand ((not openbsd)
-                    errno/EMULTIHOP errno/ENOLINK
-                    ;; STREAMS:
-                    errno/ENODATA errno/ENOSTR errno/ENOSR errno/ETIME)))
-
-     (list
-      'errno/E2BIG 'errno/EACCES 'errno/EADDRINUSE 'errno/EADDRNOTAVAIL
-      'errno/EAFNOSUPPORT 'errno/EAGAIN 'errno/EALREADY 'errno/EBADF
-      'errno/EBADMSG 'errno/EBUSY 'errno/ECANCELED 'errno/ECHILD
-      'errno/ECONNABORTED 'errno/ECONNREFUSED 'errno/ECONNRESET
-      'errno/EDEADLK 'errno/EDESTADDRREQ 'errno/EDOM 'errno/EDQUOT
-      'errno/EEXIST 'errno/EFAULT 'errno/EFBIG 'errno/EHOSTUNREACH
-      'errno/EIDRM 'errno/EILSEQ 'errno/EINPROGRESS 'errno/EINTR 'errno/EINVAL
-      'errno/EIO 'errno/EISCONN 'errno/EISDIR 'errno/ELOOP 'errno/EMFILE
-      'errno/EMLINK 'errno/EMSGSIZE 'errno/ENAMETOOLONG 'errno/ENETDOWN
-      'errno/ENETRESET 'errno/ENETUNREACH 'errno/ENFILE 'errno/ENOBUFS
-      'errno/ENODEV 'errno/ENOENT 'errno/ENOEXEC 'errno/ENOLCK 'errno/ENOMEM
-      'errno/ENOMSG 'errno/ENOPROTOOPT 'errno/ENOSPC 'errno/ENOSYS
-      'errno/ENOTCONN 'errno/ENOTDIR 'errno/ENOTEMPTY 'errno/ENOTRECOVERABLE
-      'errno/ENOTSOCK 'errno/ENOTSUP 'errno/ENOTTY 'errno/ENXIO
-      'errno/EOPNOTSUPP 'errno/EOVERFLOW 'errno/EOWNERDEAD 'errno/EPERM
-      'errno/EPIPE 'errno/EPROTO 'errno/EPROTONOSUPPORT 'errno/EPROTOTYPE
-      'errno/ERANGE 'errno/EROFS 'errno/ESPIPE 'errno/ESRCH 'errno/ESTALE
-      'errno/ETIMEDOUT 'errno/ETXTBSY 'errno/EWOULDBLOCK 'errno/EXDEV
-
-      (cond-expand ((not openbsd)
-                    'errno/EMULTIHOP 'errno/ENOLINK
-                    ;; STREAMS:
-                    'errno/ENODATA 'errno/ENOSTR 'errno/ENOSR 'errno/ETIME))
-      ))
-
-(define-record-type syscall-error
-    (make-syscall-error errno message procedure-name syscall-name data)
-    syscall-error?
-  (errno syscall-error:errno)
-  (message syscall-error:message)
-  (procedure-name syscall-error:procedure-name)
-  (syscall-name syscall-error:syscall-name)
-  (data syscall-error:data))
-
-(define (errno-error errno procedure-name syscall-name . data)
-  (raise (make-syscall-error
-          errno
-          (string-append (symbol->string procedure-name)
-                         " called "
-                         (symbol->string syscall-name)
-                         ": "
-                         (symbol->string (errno-name errno))
-                         ": "
-                         (errno-string errno))
-          procedure-name
-          syscall-name
-          data)))
-
-(define (errno-string errno)
-  (if (not (exact-integer? errno))
-      (srfi-198-error "errno-string requires an exact integer" 'errno-string errno))
-  (%strerror errno))
-
-(define (errno-name errno)
-  (if (not (exact-integer? errno))
-      (srfi-198-error "errno-name requires an exact integer" 'errno-name errno))
-  (hash-table-ref errno-map errno))
+(define (raise-foreign-error)
+  #f
+  )
